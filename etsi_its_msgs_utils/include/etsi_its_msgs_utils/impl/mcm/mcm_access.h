7c966ce7d1e67d026ae0834cb88f6ce88cf53b42
Jean-Pierre <jean-pierre.busch@ika.rwth-aachen.de>
2025-08-07 15:10:27 +0000

    Rebase from 'upstream'

--- etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/mcm/mcm_access.h
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/impl/mcm/mcm_access.h
@@ -25,19 +25,27 @@ SOFTWARE.
 */
 
 /**
- * @file spatem_ts_access.h
- * @brief Main SPATEM access header to include in ROS 1 projects
+ * @file impl/mcm/mcm_access.h
+ * @brief Main MCM access implementation header
  */
+
+#undef ETSI_ITS_MSGS_UTILS_IMPL_ASN1_PRIMITIVES_ASN1_PRIMITIVES_GETTERS_H
+#undef ETSI_ITS_MSGS_UTILS_IMPL_ASN1_PRIMITIVES_ASN1_PRIMITIVES_SETTERS_H
+#undef ETSI_ITS_MSGS_UTILS_IMPL_MCM_MCM_UTILS_H
+#undef ETSI_ITS_MSGS_UTILS_IMPL_CHECKS_H
 
 #pragma once
 
-// Messages
-#include <etsi_its_spatem_ts_msgs/SPATEM.h>
-#include <geometry_msgs/PointStamped.h>
+#include <cstring>
+#include <iostream>
+#include <map>
 
-namespace etsi_its_spatem_ts_msgs {
-    namespace gm = geometry_msgs;
-}
+#include <GeographicLib/UTMUPS.hpp>
 
-// Implementation
-#include <etsi_its_msgs_utils/impl/spatem/spatem_ts_access.h>
+#include <etsi_its_msgs_utils/impl/constants.h>
+#include <etsi_its_msgs_utils/impl/mcm/mcm_getters.h>
+#include <etsi_its_msgs_utils/impl/mcm/mcm_setters.h>
+
+namespace etsi_its_mcm_uulm_msgs::access {
+#include <etsi_its_msgs_utils/impl/mcm/mcm_utils.h>
+}  // namespace etsi_its_mcm_uulm_msgs::access
