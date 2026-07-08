37befdd9b9836f5f554c88df8467ec71ac03f930
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2026-07-08 14:50:43 +0200

    Trimmed the branch to only the etsi_its_conversion/etsi_its_denm_conversion sub directory

--- etsi_its_msgs_utils/samples/utils.py
+++
@@ -1,8 +0,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright Institute for Automotive Engineering (ika), RWTH Aachen University
-
-LEAP_SECONDS_SINCE_2004 = int(5)
-UNIX_SECONDS_2004 = int(1072915200)
-
-def get_t_its(unix_nanoseconds):
-    return int(unix_nanoseconds * 1e-6 + LEAP_SECONDS_SINCE_2004 * 1e3 - UNIX_SECONDS_2004 * 1e3)
