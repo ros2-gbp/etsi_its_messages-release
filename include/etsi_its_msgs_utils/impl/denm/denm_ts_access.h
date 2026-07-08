b6935b96110f7400eed6b94987f2c66e32601d09
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2026-07-08 15:39:55 +0200

    Rebase from 'release/kilted/etsi_its_msgs_utils'

--- include/etsi_its_msgs_utils/impl/denm/denm_ts_access.h
+++ include/etsi_its_msgs_utils/impl/denm/denm_ts_access.h
@@ -2,8 +2,8 @@
 // Copyright Institute for Automotive Engineering (ika), RWTH Aachen University
 
 /**
- * @file impl/denm/denm_access.h
- * @brief Main DENM access implementation header
+ * @file impl/denm/denm_ts_access.h
+ * @brief Main DENM TS access implementation header
  */
 
 #undef ETSI_ITS_MSGS_UTILS_IMPL_ASN1_PRIMITIVES_ASN1_PRIMITIVES_GETTERS_H
@@ -13,10 +13,10 @@
 #undef ETSI_ITS_MSGS_UTILS_IMPL_DENM_DENM_UTILS_H
 #undef ETSI_ITS_MSGS_UTILS_IMPL_CDD_CDD_GETTERS_COMMON_H
 #undef ETSI_ITS_MSGS_UTILS_IMPL_CDD_CDD_SETTERS_COMMON_H
+#undef ETSI_ITS_MSGS_UTILS_IMPL_CDD_CDD_V1_3_1_GETTERS_H
+#undef ETSI_ITS_MSGS_UTILS_IMPL_CDD_CDD_V1_3_1_SETTERS_H
 #undef ETSI_ITS_MSGS_UTILS_IMPL_CDD_CDD_V2_1_1_GETTERS_H
 #undef ETSI_ITS_MSGS_UTILS_IMPL_CDD_CDD_V2_1_1_SETTERS_H
-#undef ETSI_ITS_MSGS_UTILS_IMPL_CDD_CDD_V2_2_1_GETTERS_H
-#undef ETSI_ITS_MSGS_UTILS_IMPL_CDD_CDD_V2_2_1_SETTERS_H
 #undef ETSI_ITS_MSGS_UTILS_IMPL_CHECKS_H
 
 #pragma once
@@ -28,9 +28,9 @@
 #include <GeographicLib/UTMUPS.hpp>
 
 #include <etsi_its_msgs_utils/impl/constants.h>
-#include <etsi_its_msgs_utils/impl/denm/denm_getters.h>
-#include <etsi_its_msgs_utils/impl/denm/denm_setters.h>
+#include <etsi_its_msgs_utils/impl/denm/denm_ts_getters.h>
+#include <etsi_its_msgs_utils/impl/denm/denm_ts_setters.h>
 
-namespace etsi_its_denm_msgs::access {
+namespace etsi_its_denm_ts_msgs::access {
 #include <etsi_its_msgs_utils/impl/denm/denm_utils.h>
-}  // namespace etsi_its_denm_msgs::access
+}  // namespace etsi_its_denm_ts_msgs::access
