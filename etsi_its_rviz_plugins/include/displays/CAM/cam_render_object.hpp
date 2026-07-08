5df04662b856fcd4b056974708c25223fad1672e
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2026-07-08 15:35:23 +0200

    Rebase from 'upstream'

--- etsi_its_rviz_plugins/include/displays/CAM/cam_render_object.hpp
+++ etsi_its_rviz_plugins/include/displays/CAM/cam_render_object.hpp
@@ -1,28 +1,8 @@
-/** ============================================================================
-MIT License
-
-Copyright (c) 2023-2025 Institute for Automotive Engineering (ika), RWTH Aachen University
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-============================================================================= */
+// SPDX-License-Identifier: MIT
+// Copyright Institute for Automotive Engineering (ika), RWTH Aachen University
 
 #include "etsi_its_cam_msgs/msg/cam.hpp"
+#include "etsi_its_cam_ts_msgs/msg/cam.hpp"
 #include <geometry_msgs/msg/pose.hpp>
 #include <geometry_msgs/msg/vector3.hpp>
 #include <std_msgs/msg/header.hpp>
@@ -30,8 +10,11 @@ SOFTWARE.
 
 #include <tf2/LinearMath/Quaternion.h>
 #include <etsi_its_msgs_utils/cam_access.hpp>
+#include <etsi_its_msgs_utils/cam_ts_access.hpp>
 
 #include "rviz_common/validate_floats.hpp"
+#include <variant>
+#include <cmath>
 
 namespace etsi_its_msgs
 {
@@ -45,7 +28,18 @@ namespace displays
 class CAMRenderObject
 {
   public:
-    CAMRenderObject(etsi_its_cam_msgs::msg::CAM cam, rclcpp::Time receive_time, uint16_t n_leap_seconds=etsi_its_msgs::LEAP_SECOND_INSERTIONS_SINCE_2004.rbegin()->second);
+    CAMRenderObject(const std::variant<
+                      etsi_its_cam_msgs::msg::CAM,
+                      etsi_its_cam_ts_msgs::msg::CAM
+                    > & cam_variant,
+                    rclcpp::Time receive_time,
+                    uint16_t n_leap_seconds = etsi_its_msgs::LEAP_SECOND_INSERTIONS_SINCE_2004.rbegin()->second);
+
+    CAMRenderObject(const etsi_its_cam_msgs::msg::CAM & cam, rclcpp::Time receive_time, uint16_t n_leap_seconds = etsi_its_msgs::LEAP_SECOND_INSERTIONS_SINCE_2004.rbegin()->second)
+      : CAMRenderObject(std::variant<etsi_its_cam_msgs::msg::CAM, etsi_its_cam_ts_msgs::msg::CAM>(cam), receive_time, n_leap_seconds) {}
+
+    CAMRenderObject(const etsi_its_cam_ts_msgs::msg::CAM & cam, rclcpp::Time receive_time, uint16_t n_leap_seconds = etsi_its_msgs::LEAP_SECOND_INSERTIONS_SINCE_2004.rbegin()->second)
+      : CAMRenderObject(std::variant<etsi_its_cam_msgs::msg::CAM, etsi_its_cam_ts_msgs::msg::CAM>(cam), receive_time, n_leap_seconds) {}
 
     /**
      * @brief This function validates all float variables that are part of a CAMRenderObject
@@ -103,6 +97,13 @@ class CAMRenderObject
      */
     double getSpeed();
 
+    /**
+     * @brief Check if the CAM object is TS (release 2)
+     *
+     * @return true if CAM object is TS, false otherwise
+     */
+    bool isTS();
+
   private:
     // member variables
     std_msgs::msg::Header header;
@@ -111,7 +112,7 @@ class CAMRenderObject
     geometry_msgs::msg::Pose pose;
     geometry_msgs::msg::Vector3 dimensions;
     double speed;
-
+    bool is_ts = false;
 };
 
 }  // namespace displays
