062d45cb2d3e0172a58077f9a10801dc2f35e18e
Lennart Reiher <lennart.reiher@ika.rwth-aachen.de>
2026-07-08 14:58:39 +0200

    Rebase from 'upstream'

---
+++ etsi_its_msgs_utils/include/etsi_its_msgs_utils/denm_ts_access.hpp
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+// Copyright Institute for Automotive Engineering (ika), RWTH Aachen University
+
+/**
+ * @file denm_ts_access.hpp
+ * @brief Main DENM TS access header to include in ROS 2 projects
+ */
+
+#pragma once
+
+// Messages
+#include <etsi_its_denm_ts_msgs/msg/denm.hpp>
+#include <geometry_msgs/msg/point_stamped.hpp>
+
+namespace etsi_its_denm_ts_msgs {
+    using namespace msg;
+    namespace gm = geometry_msgs::msg;
+}
+
+// Implementation
+#include <etsi_its_msgs_utils/impl/denm/denm_ts_access.h>
